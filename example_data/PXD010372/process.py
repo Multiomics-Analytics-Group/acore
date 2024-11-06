af70100602852214ed6bff9647fa9c614404fc7c
Henry Webel <heweb@dtu.dk>
2024-11-06 13:33:36 +0100

    :sparkles: finish processing data

--- example_data/PXD010372/process.py
+++ example_data/PXD010372/process.py
@@ -4,24 +4,23 @@
 from pathlib import Path
 
 import pandas as pd
-import requests
 
-
-def download_file(url, local_filename):
-    with requests.get(url, stream=True, timeout=60) as r:
-        r.raise_for_status()
-        with open(local_filename, "wb") as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
-
+import acore.io
 
 # %%
 fpath_meta = "meta.csv"  # in same folder as this script
 furl_pg = "https://ars.els-cdn.com/content/image/1-s2.0-S0092867418311668-mmc2.xlsx"
 
+# %% [markdown]
+# Where to save the dat
+
+# %%
+dir_proc = Path("processed")
+dir_proc.mkdir(exist_ok=True)
+
 # %%
 local_filename = Path(furl_pg).name
-download_file(furl_pg, local_filename)
+acore.io.download_file(furl_pg, local_filename)
 
 # %%
 meta_patients = pd.read_csv(fpath_meta, index_col=0)
@@ -53,10 +52,21 @@ pgs = data.filter(like="Patient")
 pgs
 
 # %% [markdown]
-# 11 and 11B ?
+# There are two measurements for patient 11 in the data: 11 and 11B. In the methods of
+# the paper it is stated:
+#
+# "We required a minimum peptide ratio count of 1 to report a quantitative readout
+# and averaged the results from duplicate measurements of the same sample."
+#
+# We will do this manually for patient 11 measurements.
 
 # %%
 pgs.filter(like="Patient11").describe()
+
+# %%
+pgs = pgs.assign(Patient11=lambda df: df.filter(like="Patient11").mean(axis=1)).drop(
+    ["Patient11B"], axis=1
+)
 
 # %%
 patient_int_map = (
@@ -73,17 +83,34 @@ patient_int_map = (
 patient_int_map
 
 # %%
-meta_patients.join(patient_int_map)
+meta_patients = meta_patients.join(patient_int_map).set_index(
+    "Patient_id",
+)
+fname = dir_proc / "meta_patients.csv"
+meta_patients.to_csv(fname)
+print("Saved to ", fname)
+meta_patients
 
 # %%
-meta_pgs = data.drop(pgs.columns, axis=1)
+meta_pgs = data.drop(pgs.columns, axis=1).drop("Patient11B", axis=1)
+fname = dir_proc / "meta_pgs.csv"
+meta_pgs.to_csv(fname)
+print("Saved to ", fname)
 meta_pgs
 
 # %%
 meta_pgs.describe(exclude="number")
 
 # %%
-pgs.T
+mask = meta_pgs["Potential contaminant"].isna()
+mask.value_counts()
+
+# %%
+pgs = pgs.loc[mask].T
+fname = dir_proc / "omics.csv"
+pgs.to_csv(fname)
+print("Saved to ", fname)
+pgs
 
 # %% [markdown]
 # Need to decide how to handle the duplicated patient measurement for patient 11.
