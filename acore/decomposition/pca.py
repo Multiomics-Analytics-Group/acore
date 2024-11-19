b9b035f7d06da46d4e6a0412ca91e0bf83cb3751
Henry Webel <heweb@dtu.dk>
2024-11-19 17:16:51 +0100

    Njab stuff (#12)
    * :sparkles: Download data from PRIDE
    
    * :heavy_plus_sign: add dependencies to pkg
    
    * :memo: update documentation to include example
    
    * :art: internal renaming of vars to variables
    
    * :heavy_plus_sign: add dsp_pandas as dependency
    
    - sets up basic pandas formatting options
    
    * :bug: fix PyPI package name
    
    * :sparkles: move lower triangle correlations from njab to acore
    
    * :art: some basic plotting functionality from njab ( to be move to aviz)
    
    * :art: ignore mac specific files
    
    * :art: add hint to exploratory analysis to install acore

--- acore/decomposition/pca.py
+++ acore/decomposition/pca.py
@@ -1,10 +1,8 @@
-from typing import Optional
-
-import matplotlib
 import pandas as pd
 import sklearn.decomposition
 
 
+# ! also a version in exploratory analysis
 def run_pca(
     df_wide: pd.DataFrame, n_components: int = 2
 ) -> tuple[pd.DataFrame, sklearn.decomposition.PCA]:
@@ -35,16 +33,3 @@ def run_pca(
     ]
     PCs = pd.DataFrame(PCs, index=df_wide.index, columns=cols)
     return PCs, pca
-
-
-# ! move to aviz
-def plot_explained_variance(
-    pca: sklearn.decomposition.PCA, ax: Optional[matplotlib.axes.Axes] = None
-) -> matplotlib.axes.Axes:
-    """Plot explained variance of PCA from scikit-learn."""
-    exp_var = pd.Series(pca.explained_variance_ratio_).to_frame("explained variance")
-    exp_var.index += 1  # start at 1
-    exp_var["explained variance (cummulated)"] = exp_var["explained variance"].cumsum()
-    exp_var.index.name = "PC"
-    ax = exp_var.plot(ax=ax)
-    return ax
