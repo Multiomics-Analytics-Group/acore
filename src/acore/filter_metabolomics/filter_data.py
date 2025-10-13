81f328d1bf923e0d8215a702200a89a1e44c79fb
feliciaschulz <feliciaschulz@posteo.de>
2025-10-13 14:34:37 +0200

    Updating and cleaning up filtering docstrings

--- src/acore/filter_metabolomics/filter_data.py
+++ src/acore/filter_metabolomics/filter_data.py
@@ -1,13 +1,9 @@
-"""Module for filtering metabolomics feature table."""
+"""Functions for filtering metabolomics feature table by biologically relevant features."""
 
 import pandas as pd
 
 
-from .filter_data import filter_biological_relevance
-from .make_numeric import convert_to_numeric
-
-
-def filter_mz_rt(
+def filter_biological_relevance(
     df: pd.DataFrame,
     rt_col: str = "Average Rt(min)",
     mz_col: str = "Average Mz",
@@ -15,15 +11,9 @@ def filter_mz_rt(
     mz_low: int = None,
     rt_dead_volume: float = None,
     save_removed: bool = True,
-    print_na_summary: bool = True,
 ) -> tuple:
     """
-    This function filters rows from a data frame based on retention time and m/z and checks data types.
-    If specified by the user, it evaluates each row on whether there are NaN values and prints
-    a statement for each.
-
-    Data types:
-        Tries to convert mz and RT columns to numeric.
+    Cleans a DataFrame by filtering rows based on retention time and m/z.
 
     M/z filtering:
         Filters out all features that have m/z decimals in a given range that are below
@@ -36,8 +26,7 @@ def filter_mz_rt(
     If save_removed==True, the removed features are saved to a separate DataFrame that also
     contains a new column, "RemovalReason", indicating the removal reason.
 
-    Usage: filter_mz_rt(df, rt_col, mz_col, mz_decimals. mz_low, rt_dead_volume, save_removed, print_na_summary)
-
+    Usage: filter_biological_relevance(df, rt_col, mz_col, mz_decimals, mz_low, rt_dead_volume, save_removed)
 
     :param pd.DataFrame df: Input DataFrame.
     :param str rt_col: Column name for retention time.
@@ -52,28 +41,36 @@ def filter_mz_rt(
         - If `save_removed=False`: `(cleaned_df, None)`
     """
 
-    cols_to_convert = [rt_col, mz_col]
-    numeric_df = convert_to_numeric(
-        df, cols_to_convert=cols_to_convert, print_na_summary=True
-    )
-
-    rt_dead_volume = 0.8  # in minutes
-    mz_decimals = [0.3, 0.9]
-    mz_low = 600
-
-    filtered_df, removed_features = filter_biological_relevance(
-        numeric_df,
-        rt_col=rt_col,
-        mz_col=mz_col,
-        mz_decimals=mz_decimals,
-        mz_low=mz_low,
-        rt_dead_volume=rt_dead_volume,
-        save_removed=True,
-    )
+    removed_list = []  # store removed rows, if save_removed=True
 
     if rt_dead_volume is not None:
-        print("Filtering based on RT completed.")
-    if mz_decimals is not None and mz_low is not None:
-        print("Filtering based on m/z was completed.")
+        # Filter: rt >= 0.8
+        mask_rt = df[rt_col] < rt_dead_volume
+        if save_removed:
+            removed_rt = df.loc[mask_rt].copy()
+            removed_rt["RemovalReason"] = "BelowDeadVolume"
+            removed_list.append(removed_rt)
+        df = df.loc[~mask_rt]  # Remove from df
 
-    return filtered_df, removed_features
+    if mz_decimals and mz_low is not None:
+        # Filter: mz pattern
+        low, high = mz_decimals
+        mask_mz = (
+            (df[mz_col] < mz_low) & (df[mz_col] % 1 >= low) & (df[mz_col] % 1 <= high)
+        )
+        if save_removed:
+            removed_mz = df.loc[mask_mz].copy()
+            removed_mz["RemovalReason"] = "NotBiologicallyRelevant"
+            removed_list.append(removed_mz)
+        df = df.loc[~mask_mz]
+
+    # Combine all removed features
+    removed_df = (
+        pd.concat(
+            removed_list, ignore_index=True
+        )  # concat removed features from list to df
+        if save_removed and removed_list
+        else None
+    )
+
+    return (df, removed_df)
