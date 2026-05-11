543bdfc481d85872e396e489d7bf90b86752a9ce
feliciaschulz <feliciaschulz@posteo.de>
2026-05-11 13:44:18 +0200

    :art: put drift correction plotting functions from src to api examples

--- src/acore/drift_correction/__init__.py
+++ src/acore/drift_correction/__init__.py
@@ -6,16 +6,15 @@ Module for correcting drift (within-batch correction) in metabolomics data with 
 from .cpca_drift_correction import (
     check_missingness,
     cpca_centroid,
-    pca_for_cpca_drift,
     run_cpca_drift_correction,
 )
-from .loess_drift_correction import loess_example_curve, run_drift_correction
+
+from .loess_drift_correction import run_loess_drift_correction, qc_rlsc_loess
 
 __all__ = [
     "check_missingness",
     "cpca_centroid",
-    "pca_for_cpca_drift",
     "run_cpca_drift_correction",
-    "loess_example_curve",
-    "run_drift_correction",
+    "run_loess_drift_correction",
+    "qc_rlsc_loess",
 ]
