c8defa06bb03654ebe7812e085a0c4b6527d51de
Henry Webel <heweb@dtu.dk>
2026-06-15 11:54:45 +0200

    :memo: describe dependencies and identify core modules
    - core modules dependencies should be installed always
    - the other should only be installed on request

--- src/acore/__init__.py
+++ src/acore/__init__.py
@@ -2,7 +2,30 @@ from importlib.metadata import version
 
 import dsp_pandas  # sets up pandas formatting options
 
-from . import decomposition, imputation_analysis
+from . import (
+    batch_correction,
+    decomposition,
+    enrichment_analysis,
+    exploratory_analysis,
+    filter_metabolomics,
+    imputation_analysis,
+    io,
+    multiple_testing,
+    normalization,
+    types,
+)
 
-__all__ = ["dsp_pandas", "decomposition", "imputation_analysis"]
+__all__ = [
+    "dsp_pandas",
+    "decomposition",
+    "imputation_analysis",
+    "io",
+    "enrichment_analysis",
+    "filter_metabolomics",
+    "batch_correction",
+    "exploratory_analysis",
+    "multiple_testing",
+    "normalization",
+    "types",
+]
 __version__ = version("acore")
